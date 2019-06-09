4bf3ee3ad961aca6203700aace058ad4a760aef2
kemalcansiler <kemalcansiler@gmail.com>
2019-06-09 09:57:06 +0300

    proje build yapılabilmesi için gerekli çalışmalar yapıldı.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,8 @@ import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
 import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 import {StatusBar} from '@ionic-native/status-bar/ngx';
 
-import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {Camera} from '@ionic-native/camera/ngx';
 import {HttpClientModule} from '@angular/common/http';
 
@@ -23,9 +23,8 @@ import {HttpClientModule} from '@angular/common/http';
     providers: [
         StatusBar,
         SplashScreen,
-        {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
-
         Camera,
+        {provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
     ],
     bootstrap: [AppComponent]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -37,23 +37,19 @@ export class HomePage implements OnInit {
         this.run();
     }
 
-    async run() {
+    run() {
         const img = document.getElementById('image') as HTMLImageElement;
         // Load the model.
-        const model = await mobileNet.load(1, 0.5);
-        // Classify the image.
-        this.predictions = await model.classify(img, 4);
-        console.log('Predictions');
-        console.log(this.predictions);
-        // Get the logits.
-        const logits = model.infer(img);
-        console.log('Logits');
-        logits.print(true);
-        // Get the embedding.
-        const embedding = model.infer(img, true);
-        console.log('Embedding');
-        embedding.print(true);
-        this.textTranslate();
+        mobileNet.load(1, 0.5).then(
+            (model) => {
+                model.classify(img, 4).then(
+                    (data) => {
+                        this.predictions = data;
+                        this.textTranslate(data);
+                    }
+                );
+            }
+        );
     }
 
     takePhoto() {
@@ -72,9 +68,9 @@ export class HomePage implements OnInit {
         });
     }
 
-    textTranslate() {
+    textTranslate(predictions: any) {
         let text = '';
-        for (const item of this.predictions) {
+        for (const item of predictions) {
             text += item.className + '|';
         }
         this.http.post('https://www.icibot.net/api/text_detect?text=' + text, {
@@ -89,7 +85,9 @@ export class HomePage implements OnInit {
                 if (data != null) {
                     const tData = data.split('|');
                     for (let i = 0; i < this.predictions.length; i++) {
-                        this.predictions[i].className = tData[i];
+                        if (tData[i] != '') {
+                            this.predictions[i].className = tData[i];
+                        }
                     }
                 }
             }
